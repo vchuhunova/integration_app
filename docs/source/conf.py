850482c170eb1310e1e2c352bf6ae3e456c93e10
Piotr Schneider <piotr.schneider@poczta.umcs.lublin.pl>
2019-11-05 11:10:15 +0100

    modyfy file conf.py - change author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'myservice'
-copyright = '2017, joe'
-author = 'joe'
+copyright = '2019, ps'
+author = 'ps'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -93,7 +93,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
